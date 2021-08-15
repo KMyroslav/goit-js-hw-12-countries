3d253dce833af1691d443d8d042f5cd6174df653
KMyroslav <kukhtarukm@gmail.com>
2021-08-15 18:28:03 +0300

    Finishes logic

---
+++ src/JS/fetchCountries.js
@@ -0,0 +1,8 @@
+export default fetchCountries;
+
+function fetchCountries(searchQuery) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+    .then(r => r.json())
+    .then(([...country]) => country)
+    .catch(error => console.log(error));
+}
---
+++ src/JS/index.js
@@ -0,0 +1,38 @@
+import '../sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
+import fetchCountries from './fetchCountries';
+import coupleResultsTpl from '../templates/coupleResults.hbs';
+import singleResultTpl from '../templates/singleResult.hbs';
+const debounce = require('lodash.debounce');
+const { alert, notice, info, success, error } = require('@pnotify/core');
+
+const resultsDiv = document.querySelector('.results');
+const searchQuery = document.querySelector('#searchQuery');
+
+searchQuery.addEventListener('input', debounce(onFormInput, 500));
+
+function onFormInput(e) {
+  e.preventDefault();
+  if (searchQuery.value) {
+    fetchCountries(searchQuery.value)
+      .then(countries => {
+        if (countries.length > 1 && countries.length <= 10) {
+          resultsDiv.innerHTML = coupleResultsTpl(countries);
+          return;
+        } else if (countries.length > 10) {
+          error({
+            title: 'Whoops...',
+            text: 'Too many matches found. Please enter a more specific query',
+          });
+          return;
+        }
+        resultsDiv.innerHTML = singleResultTpl(countries[0]);
+      })
+      .catch(err => {
+        error({
+          title: 'Oops!',
+          text: 'Something went wrong, try to check if your spelling is right',
+        });
+      });
+  }
+}
